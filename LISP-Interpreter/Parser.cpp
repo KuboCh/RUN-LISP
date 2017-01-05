65bfb82a66993ed7b0c78635cef89b5e3882c175
Filip Sakala <sakalfil@fit.cvut.cz>
2017-01-05 18:51:55 +0100

    Base changes, zahrievacie kolo v C++

--- LISP-Interpreter/Parser.cpp
+++ LISP-Interpreter/Parser.cpp
@@ -14,17 +14,26 @@ string readString(string line, int from) {
     line = line.substr(1);  
 }
 
+void readList(string line){}
+void readNumber(string line){}
+
+/*
+ * Parse content of the line as:
+ * 1. Atom (Number, String, etc.)
+ * 2. Symbol -> could be evaluated as atom/another symbol/list
+ * 3. List -> functions/atoms/symbols/...
+ */
 void parse(string line) {
     switch (line[0]) {
         case '"':
             readString(line, 0);
             break;
         case '(':
-            
+            readList(line);
             break;
         default:
             if (line[0] >= '0' && line[0] <= '9'){
-                //number
+                readNumber(line);
             }
             //premenna
             break;
--- LISP-Interpreter/main.cpp
+++ LISP-Interpreter/main.cpp
@@ -4,17 +4,21 @@
 
 using namespace std;
 
+// REPL = Read Eval Print Loop
+
 int main(int argc, char** argv) {
-    
-    cout << "Welcome to LISP" << endl;
-    for (;;) {
-        string line;
-        cin>>line;
-        
+    cout << "Welcome to LISP interpreter" << endl;
+    int lineNo = 0;
+    string line;
+
+    while(true) {
+        cout << ++lineNo << ". > ";
+        getline(cin, line);
+        cout << "You said: " << line << endl;
+        if(line == "bye" || line == "exit") 
+            break;
     }
-   
-    
-    
+    cout << "Bye!";
     return 0;
 }
 
